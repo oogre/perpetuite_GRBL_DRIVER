57978ffe59e8bb3d90cfe57b533f5fc0cc09aa99
oogre <vincent@ogre.be>
2020-08-22 14:29:56 +0200

    homing tester

--- CNC_TWINS/src/GCodeHelper.js
+++ CNC_TWINS/src/GCodeHelper.js
@@ -2,7 +2,7 @@
   GCODE - gCodeHelper.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2020-08-21 19:46:37
-  @Last Modified time: 2020-08-22 12:50:25
+  @Last Modified time: 2020-08-22 14:16:33
 \*----------------------------------------*/
 
 import SerialPort from "serialport";
@@ -14,7 +14,8 @@ export default class GCodeHelper{
 		this.serialBaudrate = serialBaudrate;
 		this.verbose = verbose;
 		this.serialPort;
-		self.initialized = false;
+		this.initialized = false;
+		this.homing = false;
 		this.eventHandlers = {
 			ready : [],
 			commandDone : [],
@@ -29,12 +30,20 @@ export default class GCodeHelper{
 			if(line.includes("error")){
 				return this.triger(`error`, line);
 			}
+
 			if(!this.initialized){
 				this.initialized = true;
-				setTimeout(()=>this.triger(`ready`), 2000);
+				setTimeout(()=>{
+					this.homing = true;
+					this.send("$H");
+				}, 2000);
 			}
 			
 			if(line.includes("ok")){
+				if(this.homing){
+					this.homing = false;
+					return this.triger(`ready`);
+				}
 				this.triger(`commandDone`);
 			}
 		}
--- CNC_TWINS/src/main.js
+++ CNC_TWINS/src/main.js
@@ -3,7 +3,7 @@
   GCODE - main.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2020-08-21 17:38:22
-  @Last Modified time: 2020-08-22 12:56:11
+  @Last Modified time: 2020-08-22 14:29:41
 \*----------------------------------------*/
 
 import { program } from 'commander';
@@ -19,6 +19,7 @@ let PING_TIMEOUT_HANDLER;
 program
 	.option('-v, --verbose', 'verbose');
 
+
 program
 	.command('run')
 	.option('-sN, --synchSerialName <synchSerialName>', 'Serial name for Sync channel', "/dev/ttyAMA0")
@@ -27,7 +28,7 @@ program
 	.option('-gN, --gCodeSerialName <gCodeSerialName>', 'Serial name for GCODE channel', "/dev/ttyACM0")
 	.option('-gB, --gCodeBaudrate <gCodeBaudrate>', 'Serial baudrate for GCODE channel', 115200)
 	
-	.option('-i, --gCodeFileInput <gCodeFileInput>', 'Path of the GCODE file to send')
+	.option('-i, --gCodeFileInput <gCodeFileInput>', 'Path of the GCODE file to send', "~/GCODE/Eraser.nc")
 	.option('-t, --gCodeTimeout <gCodeTimeout>', 'Timeout to process each GCODE line', 30000)
 
 	.description('run for perpetuity in sync with another machine')
@@ -75,6 +76,7 @@ program
 					TIMEOUT_HANDLER = timeoutBuilder();
 					sendLine();
 					sendLine();
+					
 				})
 				.on(`commandDone`, () => {
 					clearTimeout(TIMEOUT_HANDLER);
@@ -93,6 +95,70 @@ program
 					clearTimeout(PING_TIMEOUT_HANDLER);
 					PING_TIMEOUT_HANDLER = pingTimeoutBuilder();
 				}).run();
+			});
+		})
+		.catch(error => {
+			console.log(error);
+			process.exit();
+		});
+	});
+
+
+program
+	.command('gCodeRun')
+	.option('-gN, --gCodeSerialName <gCodeSerialName>', 'Serial name for GCODE channel', "/dev/ttyACM0")
+	.option('-gB, --gCodeBaudrate <gCodeBaudrate>', 'Serial baudrate for GCODE channel', 115200)
+	
+	.option('-i, --gCodeFileInput <gCodeFileInput>', 'Path of the GCODE file to send', "~/GCODE/Eraser.nc")
+	.option('-t, --gCodeTimeout <gCodeTimeout>', 'Timeout to process each GCODE line', 30000)
+
+	.description('run for perpetuity without sync with another machine')
+	.action(({gCodeSerialName, gCodeBaudrate, gCodeFileInput, gCodeTimeout, ...options}) => {
+		gCodeBaudrate = parseInt(gCodeBaudrate);
+		TIMEOUT_DELAY = parseInt(gCodeTimeout);
+
+		SerialPort.list()
+		.then(serialList => {
+			const verbose = options.parent.verbose;
+			const gCodeSerial = serialList.find(detail => detail.path.includes(gCodeSerialName));
+			if(!gCodeSerial){
+				throw new Error("Unknown GRBL terminal");
+			}	
+
+			if(verbose) console.log(`Loading GCodeFile : ${gCodeFileInput}`);
+			FSHelper.loadFileInArray(gCodeFileInput)
+			.then(GCodeData => {
+				if(verbose) console.log(`GCode : `, GCodeData);
+				const gCodeHelper = new GCodeHelper({
+					serialName : gCodeSerial.path, 
+					serialBaudrate : gCodeBaudrate, 
+					verbose : verbose
+				});
+				const pingTimeoutBuilder = () => setTimeout(() => throw new Error("SYNC TIMEOUT"), syncHelper.PING_INTERVAL*1.5);
+				const timeoutBuilder = () => setTimeout(() => throw new Error("GCODE TIMEOUT"), TIMEOUT_DELAY);
+				const sendLine = () => {
+					const line = GCodeData.shift();
+					gCodeHelper.send(line);
+					GCodeData.push(line);
+				}
+					
+				gCodeHelper.on(`ready`, () => {
+					console.log("ready");
+					/*
+					sendLine();
+					TIMEOUT_HANDLER = timeoutBuilder();
+					sendLine();
+					sendLine();
+					*/
+				})
+				.on(`commandDone`, () => {
+					clearTimeout(TIMEOUT_HANDLER);
+					sendLine();
+					TIMEOUT_HANDLER = timeoutBuilder();
+				})
+				.on(`error`, error => throw new Error(error));
+				
+				gCodeHelper.run();
 			});
 		})
 		.catch(error => {
