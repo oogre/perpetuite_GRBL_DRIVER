f90572a6e8cb0c9d77f85c4d38167e565be2709c
oogre <vincent@ogre.be>
2020-08-22 17:15:43 +0200

    G28.1

--- CNC_TWINS/src/main.js
+++ CNC_TWINS/src/main.js
@@ -3,7 +3,7 @@
   GCODE - main.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2020-08-21 17:38:22
-  @Last Modified time: 2020-08-22 17:00:26
+  @Last Modified time: 2020-08-22 17:15:34
 \*----------------------------------------*/
 
 import { program } from 'commander';
@@ -112,6 +112,7 @@ program
 						IS_RUNNING = true;
 						console.log("RUN");
 						sendLine();
+						gCodeHelper.send("G28.1");
 						//TIMEOUT_HANDLER = timeoutBuilder();
 						//sendLine();
 						//sendLine();
@@ -126,115 +127,6 @@ program
 			process.exit();
 		});
 	});
-
-
-program
-	.command('syncRun')
-	.option('-sN, --synchSerialName <synchSerialName>', 'Serial name for Sync channel', "/dev/ttyAMA0")
-	.option('-sB, --synchBaudrate <synchBaudrate>', 'Serial baudrate for Sync channel', 115200)
-	
-	.description('run for perpetuity in sync with another machine')
-	.action(({synchSerialName, synchBaudrate, ...options}) => {
-		synchBaudrate = parseInt(synchBaudrate);
-
-		SerialPort.list()
-		.then(serialList => {
-			const verbose = options.parent.verbose;
-			const synchSerial = serialList.find(detail => detail.path.includes(synchSerialName));
-			
-			if(!synchSerial){
-				return kill("Unknown Sync terminal");
-			}
-			const syncHelper = new SyncHelper({
-				serialName : synchSerial.path, 
-				serialBaudrate : synchBaudrate, 
-				verbose : verbose
-			});
-			
-			const pingTimeoutBuilder = () => setTimeout(() => kill("SYNC TIMEOUT"), syncHelper.PING_INTERVAL*1.5);
-		
-			process.on('SIGINT', () => {
-					kill("kill requested")
-			});
-
-			syncHelper.on("ready", () => {
-				//gCodeHelper.run();
-			})
-			.on("ping", data => {
-				syncHelper.send("pong");
-			})
-			.on("pong", data => {
-				clearTimeout(PING_TIMEOUT_HANDLER);
-				PING_TIMEOUT_HANDLER = pingTimeoutBuilder();
-			}).run();
-		})
-		.catch(error => {
-			console.log(error);
-			process.exit();
-		});
-	});
-
-
-
-program
-	.command('gCodeRun')
-	.option('-gN, --gCodeSerialName <gCodeSerialName>', 'Serial name for GCODE channel', "/dev/ttyACM0")
-	.option('-gB, --gCodeBaudrate <gCodeBaudrate>', 'Serial baudrate for GCODE channel', 115200)
-	
-	.option('-i, --gCodeFileInput <gCodeFileInput>', 'Path of the GCODE file to send', "~/GCODE/Eraser.nc")
-	.option('-t, --gCodeTimeout <gCodeTimeout>', 'Timeout to process each GCODE line', 30000)
-
-	.description('run for perpetuity without sync with another machine')
-	.action(({gCodeSerialName, gCodeBaudrate, gCodeFileInput, gCodeTimeout, ...options}) => {
-		gCodeBaudrate = parseInt(gCodeBaudrate);
-		TIMEOUT_DELAY = parseInt(gCodeTimeout);
-
-		SerialPort.list()
-		.then(serialList => {
-			const verbose = options.parent.verbose;
-			const gCodeSerial = serialList.find(detail => detail.path.includes(gCodeSerialName));
-			if(!gCodeSerial){
-				return kill("Unknown GRBL terminal");
-			}	
-			const gCodeHelper = new GCodeHelper({
-				serialName : gCodeSerial.path, 
-				serialBaudrate : gCodeBaudrate, 
-				verbose : verbose
-			});
-
-			if(verbose) console.log(`Loading GCodeFile : ${gCodeFileInput}`);
-			FSHelper.loadFileInArray(gCodeFileInput)
-			.then(GCodeData => {
-				if(verbose) console.log(`GCode : `, GCodeData);
-				const timeoutBuilder = () => setTimeout(() => kill("GCODE TIMEOUT", gCodeHelper), TIMEOUT_DELAY);
-				const sendLine = () => {
-					const line = GCodeData.shift();
-					gCodeHelper.send(line);
-					GCodeData.push(line);
-				}
-
-				process.on('SIGINT', () => {
-  					kill("kill requested", gCodeHelper)
-				});
-
-				gCodeHelper.on(`ready`, () => {
-					sendLine();
-					TIMEOUT_HANDLER = timeoutBuilder();
-					sendLine();
-					sendLine();
-				})
-				.on(`commandDone`, () => {
-					clearTimeout(TIMEOUT_HANDLER);
-					sendLine();
-					TIMEOUT_HANDLER = timeoutBuilder();
-				})
-				.on(`error`, error => kill(error, gCodeHelper));
-				
-				gCodeHelper.run();
-			});
-		});
-	});
-
 
 program
 	.command('list')
