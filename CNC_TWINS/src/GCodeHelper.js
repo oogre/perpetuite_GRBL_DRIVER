58676bcb67576e0f1933671853f2d9bda0f59171
oogre <vincent@ogre.be>
2020-08-31 14:35:40 +0200

    log replace

--- CNC_TWINS/src/GCodeHelper.js
+++ CNC_TWINS/src/GCodeHelper.js
@@ -2,7 +2,7 @@
   GCODE - gCodeHelper.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2020-08-21 19:46:37
-  @Last Modified time: 2020-08-31 14:31:13
+  @Last Modified time: 2020-08-31 14:35:34
 \*----------------------------------------*/
 
 import SerialPort from "serialport";
@@ -52,19 +52,23 @@ class GCodeHelperTool{
 			else if(line.match(/ALARM/gi))	GCodeHelperTool.setState("ALARM");
 			else if(line.match(/DOOR/gi))	GCodeHelperTool.setState("DOOR");
 			else 							GCodeHelperTool.setState("ERROR");
+			console.log(GCodeHelperTool.MACHINE);
 		}else if(line.match(/ERROR/gi)){
 			GCodeHelperTool.setState("ERROR");
+			console.log(GCodeHelperTool.MACHINE);
 		}else if(line.match(/ALARM/gi)){
 			GCodeHelperTool.setState("ALARM");
+			console.log(GCodeHelperTool.MACHINE);
 		}else if(line.match(/OK/gi)){
 			GCodeHelperTool.MACHINE.BUFFER_LEN--;
 			if(GCodeHelperTool.MACHINE.BUFFER_LEN == 0 ){
 				GCodeHelperTool.triger("emptyBuffer");
 			}
+			console.log(GCodeHelperTool.MACHINE);
 		}
 		if(verbose){
 			console.log(`>>`, line);
-			console.log(GCodeHelperTool.MACHINE);
+			
 		}
 	}
 	static send(data, verbose=false){
