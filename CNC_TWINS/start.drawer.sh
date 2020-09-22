75d20eb253536a64cd37c4fba3f62cd0808facc8
oogre <vincent@ogre.be>
2020-09-22 13:38:05 +0200

    runCNCtwins + debug

--- CNC_TWINS/start.drawer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# @Author: Evrard Vincent
-# @Date:   2020-08-31 17:45:16
-# @Last Modified by:   ogre
-# @Last Modified time: 2020-08-31 17:45:33
-
-
-/usr/bin/cncTwins run -gI ~/GCODE/Drawer.nc
